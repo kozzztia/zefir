7df5016002af5206404aaa4b08464f8f3230d43a
Kostia <kozzztia@gmail.com>
2026-04-11 16:20:28 +0300

    single page promos  and  services

--- single.php
+++ single.php
@@ -16,24 +16,24 @@ function zefir_render_content() {
         ],
     ]);
 
-    get_template_part('views/blocks/banner-slider', null, [
-        'field' => [
-            'type' => 'service',
-        ],
-    ]);
-
-    get_template_part('views/blocks/product-gallery', null, [
-        'field' => [
-            'type'       => 'promo',
-            'title'      => 'promo',
-            'subtitle'   => 'we can',
-            'count'      => 3,
-            'link'       => [
-                'url' => '/promo',
-                'title' => 'all promos',
-            ],
-        ],
-    ]);
+//    get_template_part('views/blocks/banner-slider', null, [
+//        'field' => [
+//            'type' => 'service',
+//        ],
+//    ]);
+//
+//    get_template_part('views/blocks/product-gallery', null, [
+//        'field' => [
+//            'type'       => 'promo',
+//            'title'      => 'promo',
+//            'subtitle'   => 'we can',
+//            'count'      => 3,
+//            'link'       => [
+//                'url' => '/promo',
+//                'title' => 'all promos',
+//            ],
+//        ],
+//    ]);
 }
 
 get_template_part('views/overall/layout');
--- views/blocks/single-item.php
+++ views/blocks/single-item.php
@@ -10,12 +10,14 @@
          ?>')"
     >
         <?php $post_type = get_post_type(); ?>
-        <?php if ( $post_type == 'product'): ?>
+
+        <?php if ( $post_type == 'product' || $post_type == 'promo' || $post_type == 'service' ) : ?>
         <?php while (have_posts()): the_post(); ?>
         <div class="item item-<?php echo get_post_type(); ?>">
 
             <?php
-            $images = get_field(get_post_type() . '_images');
+            $images = get_field('product_images');
+            $info = get_field('product_info');
             if ($images): ?>
                 <div class="item-list">
                     <?php foreach ($images as $index => $image): ?>
@@ -33,7 +35,7 @@
                         <?php if (!empty($image['url'])): ?>
                             <?php $id = 'gallery-' . $index; ?>
                             <label class="item-images-item">
-                                <input type="radio" id="<?php echo $id; ?>" name="gallery" <?php echo (int) filter_var($index, FILTER_SANITIZE_NUMBER_INT) ==  1 ? 'checked' : ''; ?> hidden>
+                                <input type="radio" id="<?php echo $id; ?>" name="gallery" <?php echo $index === 'image_1' ? 'checked' : ''; ?> hidden>
                                 <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
                             </label>
                         <?php endif; ?>
@@ -41,13 +43,46 @@
                 </div>
             <?php endif; ?>
 
+            <?php if (empty($images)): ?>
+                <div class="item-images">
+                    <div class="item-images-image">
+                        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full') ?>" alt="<?php the_title(); ?>"/>
+                    </div>
+                </div>
+            <?php endif; ?>
+
             <div class="item-content">
                 <h1 class="item-content-title"><?php the_title(); ?></h1>
-                <div class="typo item-content-text">
-                    <?php the_content(); ?>
+
+                <?php if (get_the_content()): ?>
+                    <div class="item-content-text">
+                        <?php the_content(); ?>
+                    </div>
+                <?php endif; ?>
+
+                <div class="item-content-info">
+                    <?php if (!empty($info['price'])): ?>
+                        <p><b>Price:</b> <?php echo esc_html($info['price']); ?>$</p>
+                    <?php endif; ?>
+                    <?php if (!empty($info['description'])): ?>
+                        <p><b>Description:</b> <?php echo esc_html($info['description']); ?></p>
+                    <?php endif; ?>
+                    <?php if (!empty($info['weight'])): ?>
+                        <p><b>Weight:</b> <?php echo esc_html($info['weight']); ?>kg</p>
+                    <?php endif; ?>
+                    <?php if (!empty($info['taste'])): ?>
+                        <p><b>Taste:</b> <?php echo esc_html($info['taste']); ?></p>
+                    <?php endif; ?>
+                </div>
+
+                <div class="item-content-buttons">
+                    <button type="button" class="item-content-button btn white" aria-label="send  to cart">
+                        Add to cart
+                    </button>
                 </div>
             </div>
         </div>
+
         <?php endwhile; ?>
         <?php endif; ?>
     </div>
